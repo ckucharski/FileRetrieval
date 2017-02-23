523430cc3883b6b91b783de709beb9bcc220422f
ckucharski <ckucharski@etud.univ-paris8.fr>
2017-02-23 20:26:58 +0100

    Inteface (images/texts) + Listener

---
+++ FileRetrieval/src/FileOrDirectory.java
@@ -0,0 +1,15 @@
+
+public class FileOrDirectory {
+
+	int id;
+	boolean isADirectory;
+	String name;
+	int x, y;
+	
+	public FileOrDirectory( int id, boolean isADirectory, String name )
+	{
+		this.id = id;
+		this.isADirectory = isADirectory; 
+		this.name = name;
+	}
+}
